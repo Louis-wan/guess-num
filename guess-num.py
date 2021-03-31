35f866e774175dcb28d88b02f7c15e5251a232f4
Louis-wan <louis10508@gmail.com>
2021-03-30 20:33:31 -0700

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,6 +1,11 @@
 import random
-r = random.randint(1,100)
+start = input('請start ')
+end =  input ('請end num')
+start = int (start)
+end = int (end)
+r = random.randint(start,end)
 count = 0 
+
 while True: 
 	count = count + 1
 	num = input('請猜數字 ')
